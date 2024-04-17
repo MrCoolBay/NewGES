948acfe642b9e1b5faa8c07472223de8a90df914
yxmmy <alexisdervax@gmail.com>
2024-04-17 12:12:03 +0200

    rendu final page support

--- Code/views/scolarite/supports.php
+++ Code/views/scolarite/supports.php
@@ -22,18 +22,14 @@
             <div class="home-content">
                 <i class="fa-solid fa-bars"></i>
                 <span class="text">Liste des supports </span>
-    <div class="grand-rectangle">
-        <img id="lan" src="assets\img\3.png" alt="lan valo">
-        <img id="eductive" src="assets\img\1.png" alt="logo ecoles">
-        <img id="porte-ouverte" src="assets\img\2.png" alt="porte ouverte">
-    </div>
-
-    <div id="telechargements">
-        <h2>Documents disponibles :</h2>
+            </div>
+            <br>
+    <div class="telechargements">
         <table border="3">
             <tr>
-                <th>Nom doc.</th>
-                <th>Type doc.</th>
+                <th>Nom du document</th>
+                <th>Type du document</th>
+                <th>Télécharger</th>
             </tr>
             <tr>
                 <td>Document Uno</td>
@@ -48,7 +44,7 @@
             <tr>
                 <td>Document Tres</td>
                 <td>Zip</td>
-                <td><a href="documents/document3.zip" download>Télécharger</a></td>
+                <td><a href="#" download>Télécharger</a></td>
             </tr>
             
         </table>
