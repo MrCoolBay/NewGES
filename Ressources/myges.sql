72bdafc4b1061cb5773f31d897188fcf8bd65b18
MrCoolBay <100962538+MrCoolBay@users.noreply.github.com>
2024-04-21 19:13:13 +0200

    BDD

--- Ressources/myges.sql
+++ Ressources/myges.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost
--- Généré le : jeu. 18 avr. 2024 à 15:51
+-- Généré le : dim. 21 avr. 2024 à 19:12
 -- Version du serveur : 10.4.28-MariaDB
 -- Version de PHP : 8.2.4
 
@@ -55,7 +55,13 @@ INSERT INTO `note` (`id_note`, `matiere`, `intervenant`, `note1`, `info_note1`, 
 (20, 'Anglais', 'Thomas Pierson', '15', 'salut', NULL, NULL, NULL, '2024-04-18 11:21:31', 2, NULL, NULL),
 (21, 'Anglais', 'Thomas Pierson', '15', 'salut', NULL, NULL, NULL, '2024-04-18 11:22:17', 2, NULL, NULL),
 (22, 'Anglais', 'Thomas Pierson', '15', 'weeeeee', NULL, NULL, NULL, '2024-04-18 11:23:21', 2, NULL, NULL),
-(23, 'Anglais', 'Thomas Pierson', '2', 'Salut', NULL, NULL, NULL, '2024-04-18 11:42:08', 10, NULL, NULL);
+(23, 'Anglais', 'Thomas Pierson', '2', 'Salut', NULL, NULL, NULL, '2024-04-18 11:42:08', 10, NULL, NULL),
+(24, 'Développement web :  html, css, php', 'Thomas Pierson', '15', 'Contrôle', NULL, NULL, NULL, '2024-04-18 17:18:02', 11, NULL, NULL),
+(25, 'Réseaux', 'Thomas Pierson', '12', 'TP Réseaux', NULL, NULL, NULL, '2024-04-18 17:19:06', 11, NULL, NULL),
+(26, 'Virtualisation', 'Thomas Pierson', '16', 'TD Virtu', NULL, NULL, NULL, '2024-04-18 17:19:22', 11, NULL, NULL),
+(27, 'Français', 'Thomas Pierson', '20', 'je suis bon en fr', NULL, NULL, NULL, '2024-04-18 17:19:36', 11, NULL, NULL),
+(28, 'Virtualisation', 'Thomas Pierson', '10', 'Pas ouf', NULL, NULL, NULL, '2024-04-19 10:05:12', 17, NULL, NULL),
+(29, 'Projet annuel', 'Thomas Pierson', '20', 'Bonne note', NULL, NULL, NULL, '2024-04-19 12:41:11', 18, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -79,7 +85,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id_user`, `name`, `surname`, `pdp`, `email`, `password`, `salt`, `school`) VALUES
-(1, 'Fabien', 'Lubin', 'https://media.discordapp.net/attachments/918602034301784145/1224731617264664616/IMG_2314.JPG?ex=661e8f33&is=660c1a33&hm=9d8c52f5213423bdef08aa9055405ecaa298c7c83fb1ed73d93d9ca0110f3821&=&format=webp&width=395&height=702', 'flubin1@myges.fr', '$2y$10$IhcJ1ZKKuc3hCIxj0yzx2OUnQa3A1koiKUl7jv8RgaFz9xw.Pibp2', '846279938660c2cc8149e61.97265511', 'admin'),
+(1, 'Fabien', 'Lubin', 'https://media.discordapp.net/attachments/779025361596842029/1230800965112954951/image.png?ex=6634a3b9&is=66222eb9&hm=772f76065fb80fcf52d2e2bb42bf524b0f48c647f142d16cbb019c919b4e6b67&=&format=webp&quality=lossless&width=648&height=700', 'flubin1@myges.fr', '$2y$10$IhcJ1ZKKuc3hCIxj0yzx2OUnQa3A1koiKUl7jv8RgaFz9xw.Pibp2', '846279938660c2cc8149e61.97265511', 'admin'),
 (2, 'Alexis', 'Derveaux', 'https://media.discordapp.net/attachments/1167492339200172139/1222848726951788594/FF466658-70A5-441E-87E4-67550E918643.jpg?ex=6617b59f&is=6605409f&hm=1b2c381876569937224864f57ba8f7bcf409142456763178d8ef44ff273a2d3f&=&format=webp&width=395&height=702', 'alexis@gmail.com', '$2y$10$PMseVDBpHq7jfJ6j95jR/uP/Tg9hZhCflc.cefuuuVhtH2zER9j6.', '936870711660c2da4b9baa6.61185934', 'admin'),
 (4, 'Amaury', 'Marchand', 'https://media.licdn.com/dms/image/D4E35AQEE5JtLcYxhPQ/profile-framedphoto-shrink_800_800/0/1706560499281?e=1712682000&v=beta&t=zaPGIrVJynFrngZsEgLb_o-w3sImHi1qp392-5mai5o', 'amaury@gmail.com', '$2y$10$e01uVfvocnIlQ0/v5eI1Jep9KO/0JVFzgixAfWrZm1pSTOKj3.OT6', '1331194036660c306f4db8e0.60802392', 'admin'),
 (10, 'Gaetan', 'Majkowiez', 'https://media.istockphoto.com/id/1223671392/fr/vectoriel/photo-de-profil-par-d%C3%A9faut-avatar-photo-placeholder-illustration-de-vecteur.jpg?s=612x612&w=0&k=20&c=iLDNfo8MGvF_Srti46vL4iyYbHB4bUK5iv6V7c4Pj80=', 'gmajkowiez@myges.fr', '$2y$10$FLMACAUK/dXsypRWf0OJneQnj.ebALxmf1MB4vmNLdnhz4L3zquKe', '579335945661fb81cda5a38.59436732', 'admin'),
@@ -88,7 +94,9 @@ INSERT INTO `users` (`id_user`, `name`, `surname`, `pdp`, `email`, `password`, `
 (13, 'Pierre', 'Postal', 'https://media.discordapp.net/attachments/1164881520234868787/1230501114009354240/7F4D3871-986E-4CF2-815A-FD025A3EC93F_1_105_c.jpeg?ex=66338c77&is=66211777&hm=b726cb122717780f0e2e26a7ea93d8b388e537c9e8055efd5c7c3772adb19e8d&=&format=webp&width=550&height=976', 'ppostal@myges.fr', '$2y$10$ZA4a96TlBtaP1lWGvs87HOiPFxM1brkEXnZyKMuXctsxq7dlma66i', '75058694666210cdc259267.15247958', 'ESGI'),
 (14, 'Sarah', 'Muszalski', 'https://media.licdn.com/dms/image/D4E35AQF25QjGu88ebg/profile-framedphoto-shrink_100_100/0/1709044408213?e=1714050000&v=beta&t=dUi2WDvJIC0zOqijAhX07cx73s8Jv3rzmLIvxiKsMpY', 'smuszalski@myges.fr', '$2y$10$IAXe91GXV.kPrJBZpvLeU.MgPb5Yddp0CVc2KmvEP9NW948AKqgsy', '1127611833662116b748ff50.88570720', 'EFAB'),
 (15, 'Apolline', 'Goin', 'https://media.istockphoto.com/id/1223671392/fr/vectoriel/photo-de-profil-par-d%C3%A9faut-avatar-photo-placeholder-illustration-de-vecteur.jpg?s=612x612&w=0&k=20&c=iLDNfo8MGvF_Srti46vL4iyYbHB4bUK5iv6V7c4Pj80=', 'agoin@myges.fr', '$2y$10$lXHfroN92Zw0azs.com50elBiKIMskX/QGma3fZvD375rEtRHNOzq', '1181633869662119612e1969.78712026', 'PPA'),
-(16, 'Aline', 'Chaffangeon', 'https://media.istockphoto.com/id/1223671392/fr/vectoriel/photo-de-profil-par-d%C3%A9faut-avatar-photo-placeholder-illustration-de-vecteur.jpg?s=612x612&w=0&k=20&c=iLDNfo8MGvF_Srti46vL4iyYbHB4bUK5iv6V7c4Pj80=', 'achaffangeon@myges.fr', '$2y$10$8rskMpwzEaWEs65RZMcewu5BWaZFzq2rQxK34J5wVr2yGZK9SkgAC', '79801979066211a05f10491.59587154', 'admin');
+(16, 'Aline', 'Chaffangeon', 'https://media.istockphoto.com/id/1223671392/fr/vectoriel/photo-de-profil-par-d%C3%A9faut-avatar-photo-placeholder-illustration-de-vecteur.jpg?s=612x612&w=0&k=20&c=iLDNfo8MGvF_Srti46vL4iyYbHB4bUK5iv6V7c4Pj80=', 'achaffangeon@myges.fr', '$2y$10$8rskMpwzEaWEs65RZMcewu5BWaZFzq2rQxK34J5wVr2yGZK9SkgAC', '79801979066211a05f10491.59587154', 'admin'),
+(17, 'cacaa', 'boudin', 'https://media.istockphoto.com/id/1223671392/fr/vectoriel/photo-de-profil-par-d%C3%A9faut-avatar-photo-placeholder-illustration-de-vecteur.jpg?s=612x612&w=0&k=20&c=iLDNfo8MGvF_Srti46vL4iyYbHB4bUK5iv6V7c4Pj80=', 'caca@gmail.com', '$2y$10$vvK1TN0.eDnQhvrg5iRGWupS6XFAGRiKt0Tj8ELcXZE5wjctrpiqW', '216494775662241399d1db4.02421954', 'ESUPCOM'),
+(18, 'toto', 'toto', 'https://media.istockphoto.com/id/1223671392/fr/vectoriel/photo-de-profil-par-d%C3%A9faut-avatar-photo-placeholder-illustration-de-vecteur.jpg?s=612x612&w=0&k=20&c=iLDNfo8MGvF_Srti46vL4iyYbHB4bUK5iv6V7c4Pj80=', 'tata@gmail.com', '$2y$10$3aUFSVuRw6NQnUOYDqViw.BaMHCMNniMQf4jLNKj5sQ0uezE0suY2', '147159845662265a294c698.20698701', 'ESUPCOM');
 
 --
 -- Index pour les tables déchargées
@@ -116,13 +124,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT pour la table `note`
 --
 ALTER TABLE `note`
-  MODIFY `id_note` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `id_note` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 
 --
 -- AUTO_INCREMENT pour la table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- Contraintes pour les tables déchargées
